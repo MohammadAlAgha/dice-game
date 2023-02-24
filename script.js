ceddb1f5aa023ae775465a3546cbb615cb7eb312
MohammadAlAgha <mohalagha11@gmail.com>
2023-02-24 22:44:05 +0200

    Added shuffle button option commented

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let player1 = document.getElementById("player1");
 let player2 = document.getElementById("player2");
 let score = document.getElementById("score");
-
+// button.addEventListener("click", shuffle);
 shuffle();
 
 function shuffle() {
